d66b5dd2a7f0af1d28a904f950387e62f98a1b49
genieu99 <yujin0419@sookmyung.ac.kr>
2024-07-03 11:33:12 +0900

    assignment: UserDetailsImpl로 변경 #4

--- jblog5/src/main/java/com/poscodx/jblog/security/UserDetailsServiceImpl.java
+++ jblog5/src/main/java/com/poscodx/jblog/security/UserDetailsServiceImpl.java
@@ -15,6 +15,6 @@ public class UserDetailsServiceImpl implements UserDetailsService {
 
 	@Override
 	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-		return userRepository.findById(username);
+		return userRepository.findById(username, UserDetailsImpl.class);
 	}
 }
